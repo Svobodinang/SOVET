856f485db6307628d9d582f4c0903c974e7db756
Nadezhda <svobodinang@gmail.com>
2020-03-14 17:16:43 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <topMenu />
+    <main>
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+import topMenu from '@/components/topMenu.vue'
+
+export default {
+  components: {
+    topMenu
+  }
+}
+</script>
+
+<style>
+</style>
